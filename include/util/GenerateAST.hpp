4e89a3c0ce7b3fd869dfc0634768e36ad16c080f
saadhussain31 <saadnasir31@gmail.com>
2023-07-30 04:33:41 -0400

    Generate boidlerplate classes using GenerateAST util

--- include/util/GenerateAST.hpp
+++ include/util/GenerateAST.hpp
@@ -1,17 +1,50 @@
-#ifndef GENETATE_AST_H
-#define GENETATE_AST_H
+/**
+ * @file GenerateAST.hpp
+ * @brief Header file for the GenerateAST utility class for Lox interpreter.
+ */
+
+#ifndef GENERATE_AST_HPP
+#define GENERATE_AST_HPP
 
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
 #include <sstream>
-#include <any>
+#include <string>
+#include <vector>
 
 namespace util::interpreter {
-    class GenerateAST {
-    public:
-        void init(int argc, char* argv[]);
-    };
-}
 
+/**
+ * @brief Utility class for generating AST expressions in C++.
+ */
+class GenerateAST {
+public:
+    /**
+     * @brief Main entry point to generate AST classes.
+     * @param argc The number of command-line arguments.
+     * @param argv An array of command-line arguments.
+     */
+    void init(int argc, char* argv[]);
+
+public:
+    /**
+     * @brief Defines the AST classes and generates the .cpp file.
+     * @param output_dir The output directory for the generated files.
+     * @param base_name The base name for the AST classes.
+     * @param types A vector of strings defining each AST class and its fields.
+     */
+    void define_ast(const std::string& output_dir, const std::string& base_name, const std::vector<std::string>& types);
+
+    /**
+     * @brief Defines a specific AST class and writes it to the output file.
+     * @param writer The output file stream.
+     * @param base_name The base name for the AST classes.
+     * @param class_name The name of the current AST class being defined.
+     * @param field_list The list of fields for the current AST class.
+     */
+    void define_type(std::ofstream& writer, const std::string& base_name, const std::string& class_name, const std::string& field_list);
+};
+
+} //end util::interpreter
+
+#endif // GENERATE_AST_HPP
--- src/util/GenerateAST.cpp
+++ src/util/GenerateAST.cpp
@@ -8,13 +8,98 @@
  * @date 28-07-2023
  */
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include "util/GenerateAST.hpp"
 
-using namespace util::interpreter;
+using namespace util::interpreter; 
 
 void GenerateAST::init(int argc, char* argv[]) {
-    if(argc < 2 ) {
-        std::cout << "Usage: generate_ast <output directory>\n";
+    if (argc != 2) {
+        std::cerr << "Usage: generate_ast <output directory>\n";
+        exit(64);
     }
     std::string output_dir = argv[1];
+    define_ast(output_dir, "Expr", {
+        "Binary   : Expr* left, Token* operator, Expr* right",
+        "Grouping : Expr* expression",
+        "Literal  : std::any value",
+        "Unary    : Token* operator, Expr* right"
+    });
+}
+
+
+void GenerateAST::define_ast(const std::string& output_dir, const std::string& base_name, const std::vector<std::string>& types) {
+        std::string path = output_dir + "/" + base_name + ".cpp";
+        std::ofstream writer(path);
+
+        writer << "#include \"" << base_name << ".hpp\"\n";
+        writer << "#include <vector>\n";
+        writer << "#include <std::any>\n";
+        writer << "namespace lox::interpreter {\n";
+        writer << "\n";
+
+        // Abstract base class
+        writer << "class " << base_name << " {\n";
+        writer << "public:\n";
+        writer << "    virtual ~" << base_name << "() {}\n";
+        writer << "};\n";
+        writer << "\n";
+
+        for (const std::string& type : types) {
+            std::string class_name = type.substr(0, type.find(":")); // Remove leading/trailing spaces
+            class_name.erase(0, class_name.find_first_not_of(" \t\n\r\f\v"));
+            class_name.erase(class_name.find_last_not_of(" \t\n\r\f\v") + 1);
+
+            std::string fields = type.substr(type.find(":") + 1);
+
+            define_type(writer, base_name, class_name, fields);
+        }
+
+        writer << "}\n";
+        writer.close();
+}
+
+void GenerateAST::define_type(std::ofstream& writer, const std::string& base_name, const std::string& class_name,
+const std::string& field_list) {
+        writer << "class " << class_name << " : public " << base_name << " {\n";
+        writer << "public:\n";
+
+        // Constructor.
+        writer << "    " << class_name << "(" << field_list << ") : ";
+        std::stringstream ss(field_list);
+        std::string field;
+        while (std::getline(ss, field, ',')) {
+            std::string name = field.substr(field.find(" ") + 1); // Remove leading/trailing spaces
+            name.erase(0, name.find_first_not_of(" \t\n\r\f\v"));
+            name.erase(name.find_last_not_of(" \t\n\r\f\v") + 1);
+
+            writer << name << "(" << name << ")";
+            if (!ss.eof()) {
+                writer << ", ";
+            }
+        }
+        writer << " {}\n";
+
+        // Fields.
+        writer << "\n";
+        ss.clear();
+        ss.str(field_list);
+        while (std::getline(ss, field, ',')) {
+            writer << "    " << field << ";\n";
+        }
+
+        writer << "};\n";
+        writer << "\n";
+}
+
+
+
+int main(int argc, char* argv[]) {
+    GenerateAST ast;
+    ast.init(argc, argv);
+    return 0;
 }
